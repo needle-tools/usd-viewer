ae651900b2e739175640b033fa983d0cab1701ff
Glitch (glitch-hello-website) <none>
2021-05-27 14:34:27 +0000

    Exporting to glitchdotcom/glitch-hello-website ./script.js:31317407/1858

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ Make the "Click me!" button rotate when the visitor clicks it:
 */
 const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
-btn.onclick = function() {
-  // The JS works in conjunction with the 'rotated' code in style.css 
-  btn.classList.toggle('rotated');
-};
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'rotated' code in style.css
+    btn.classList.toggle("rotated");
+  };
+}
 
 // This is a single line JS comment
 /*
