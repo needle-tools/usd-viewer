f551488d488d1a7dbd20f060bf8586ac82705a1c
hybridherbst <felix.herbst@gmail.com>
2023-08-30 17:04:10 +0200

    Various rendering fixes
    fix alpha channel rgb and a texture check
    fix opacityThreshold == 0
    fix roughness in some cases
    prepare for texture channel swizzling and combining
    fix texture color spaces
    fix material and mesh names being empty
    disable ACES for now

--- public/ThreeJsRenderDelegate.js
+++ public/ThreeJsRenderDelegate.js
@@ -70,6 +70,7 @@ class TextureRegistry {
   
           // onLoad callback
           (texture) => {
+            texture.name = filename;
             textureResolve(texture);
           },
   
@@ -137,6 +138,14 @@ class HydraMesh {
     this._mesh = new THREE.Mesh( this._geometry, material );
     this._mesh.castShadow = true;
     this._mesh.receiveShadow = true;
+
+    // ID can contain paths, we strip those here
+    let _name = id;
+    let lastSlash = _name.lastIndexOf('/');
+    if (lastSlash >= 0) {
+      _name = _name.substring(lastSlash + 1);
+    }
+    this._mesh.name = _name;
     
     window.usdRoot.add(this._mesh); // FIXME
   }
@@ -289,6 +298,12 @@ class HydraMaterial {
     'opacity': 'alphaMap'
   };
 
+  static usdPreviewToEncodingMap = {
+    'diffuseColor': THREE.sRGBEncoding,
+    'emissiveColor': THREE.sRGBEncoding,
+    'opacity': THREE.sRGBEncoding,
+  };
+
   static channelMap = {
     // Three.js expects many 8bit values such as roughness or metallness in a specific RGB texture channel.
     // We could write code to combine multiple 8bit texture files into different channels of one RGB texture where it
@@ -352,9 +367,11 @@ class HydraMaterial {
   }
   
   assignTexture(mainMaterial, parameterName) {
+    return new Promise((resolve, reject) => {
     const materialParameterMapName = HydraMaterial.usdPreviewToMeshPhysicalTextureMap[parameterName];
     if (materialParameterMapName === undefined) {
       console.warn(`Unsupported material texture parameter '${parameterName}'.`);
+      resolve();
       return;
     }
     if (mainMaterial[parameterName] && mainMaterial[parameterName].nodeIn) {
@@ -364,7 +381,7 @@ class HydraMaterial {
 
       // For debugging
       const matName = Object.keys(this._nodes).find(key => this._nodes[key] === mainMaterial);
-      console.log(`Setting texture '${materialParameterMapName}' (${textureFileName}) of material '${matName}'...`);
+      console.log(`Setting texture '${materialParameterMapName}' (${textureFileName}) of material '${matName}'... with channel '${channel}'`);
 
       this._interface.registry.getTexture(textureFileName).then(texture => {
         // console.log("getTexture", texture, nodeIn);
@@ -375,33 +392,67 @@ class HydraMaterial {
           // expects for alpha maps (not supported at the moment).
           // NOTE that this only works if diffuse maps are always set before opacity maps, so the order of
           // 'assingTexture' calls for a material matters.
-          if (textureFileName === mainMaterial.diffuseColor?.nodeIn?.file && channel === 'a') {
+          if (nodeIn.file === mainMaterial.diffuseColor?.nodeIn?.file && channel === 'a') {
             this._material.map.format = THREE.RGBAFormat;
           } else {
             // TODO: Extract the alpha channel into a new RGB texture.
+            console.warn("Separate alpha channel is currently not supported.", nodeIn.file, mainMaterial.diffuseColor?.nodeIn?.file, channel);
           }
 
           if (!this._material.alphaClip)
             this._material.transparent = true;
+          
           this._material.needsUpdate = true;
+          resolve();
           return;
         } else if (materialParameterMapName === 'metalnessMap') {
           this._material.metalness = 1.0;
         } else if (materialParameterMapName === 'roughnessMap') {
-          this._material.roughness = 0.0;
+          this._material.roughness = 1.0;
         } else if (materialParameterMapName === 'emissiveMap') {
           this._material.emissive = new THREE.Color(0xffffff);
         } else if (!HydraMaterial.channelMap[channel]) {
           console.warn(`Unsupported texture channel '${channel}'!`);
+          resolve();
           return;
         }
 
+        // TODO need to apply bias/scale to the texture in some cases.
+        // May be able to extract that for metalness/roughness/opacity/normalScale
+
         // Clone texture and set the correct format.
         const clonedTexture = texture.clone();
+        let targetSwizzle = 'rgba';
+
+        if (materialParameterMapName == 'roughnessMap' && channel != 'g') {
+          targetSwizzle = '0' + channel + '11';
+        }
+        if (materialParameterMapName == 'metalnessMap' && channel != 'b') {
+          targetSwizzle = '01' + channel + '1';
+        }
+        if (materialParameterMapName == 'occlusionMap' && channel != 'r') {
+          targetSwizzle = channel + '111';
+        }
+        if (materialParameterMapName == 'opacityMap' && channel != 'a') {
+          targetSwizzle = channel + channel + channel + channel;
+        }
+
+        clonedTexture.encoding = HydraMaterial.usdPreviewToEncodingMap[parameterName] || THREE.LinearEncoding;
+        
+        // console.log("Cloned texture", clonedTexture, "swizzled with", targetSwizzle);
+        // clonedTexture.image = HydraMaterial._swizzleImageChannels(clonedTexture.image, targetSwizzle);
+        // if (materialParameterToTargetChannel[materialParameterMapName] && channel != materialParameterToTargetChannel[materialParameterMapName])
+        if (targetSwizzle != 'rgba')
+        {
+          clonedTexture.image = HydraMaterial._swizzleImageChannels(clonedTexture.image, targetSwizzle);
+        }
+
+        // clonedTexture.image = HydraMaterial._swizzleImageChannels(clonedTexture.image, channel, 'g')
+
         clonedTexture.format = HydraMaterial.channelMap[channel];
         // clonedTexture.encoding = THREE.LinearEncoding;
         clonedTexture.needsUpdate = true;
-        
+
         if (nodeIn.st && nodeIn.st.nodeIn) {
           const uvData = nodeIn.st.nodeIn;
           // console.log("Tiling data", uvData);
@@ -438,14 +489,113 @@ class HydraMaterial {
         clonedTexture.wrapS = this.convertWrap(nodeIn.wrapS);
         clonedTexture.wrapT = this.convertWrap(nodeIn.wrapT);
 
+        if (debugTextures) console.log("Setting texture " + materialParameterMapName + " to", clonedTexture)
         this._material[materialParameterMapName] = clonedTexture;
-
         this._material.needsUpdate = true;
+
+        if (debugTextures) console.log("RESOLVED TEXTURE", matName, parameterName);
+        resolve();
+        return;
       });
     } else {
       this._material[materialParameterMapName] = undefined;
+      resolve();
+      return;
     }
+  })
   }
+
+  /**
+   * Swizzle image channels (e.g. move red channel to green channel)
+   * @param {*} image three.js image
+   * @param {string} swizzle For example, "rgga". Must have max. 4 components. Can contain 0 and 1, e.g. "rgba1" is valid.
+   * @returns three.js image
+   */
+  static _swizzleImageChannels( image, swizzle ) {
+		if ( ( typeof HTMLImageElement !== 'undefined' && image instanceof HTMLImageElement ) ||
+			( typeof HTMLCanvasElement !== 'undefined' && image instanceof HTMLCanvasElement ) ||
+			( typeof ImageBitmap !== 'undefined' && image instanceof ImageBitmap ) ) {
+
+			const canvas = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'canvas' );
+
+			canvas.width = image.width;
+			canvas.height = image.height;
+
+			const context = canvas.getContext( '2d' );
+			context.drawImage( image, 0, 0, image.width, image.height );
+
+			const imageData = context.getImageData( 0, 0, image.width, image.height );
+			const data = imageData.data;
+      
+      // console.log(data);
+      
+      const swizzleToIndex = {
+        'r': 0,
+        'g': 1,
+        'b': 2,
+        'a': 3,
+        'x': 0,
+        'y': 1,
+        'z': 2,
+        'w': 3,
+        '0': 4, // set to 0
+        '1': 5, // set to 1
+        '-': -1, // passthrough
+      };
+      const arrayAccessBySwizzle = [4,4,4,4]; // empty value if nothing defined in the swizzle pattern
+      for (let i = 0; i < swizzle.length; i++) {
+        arrayAccessBySwizzle[i] = swizzleToIndex[swizzle[i]];
+      }
+
+      const dataEntry = data.slice(0);
+			for ( let i = 0; i < data.length; i += 4 ) {
+        dataEntry[0] = data[i];
+        dataEntry[1] = data[i+1];
+        dataEntry[2] = data[i+2];
+        dataEntry[3] = data[i+3];
+        dataEntry[4] = 0; // empty value
+        dataEntry[5] = 1;
+
+        const rAccess = arrayAccessBySwizzle[0];
+        const gAccess = arrayAccessBySwizzle[1];
+        const bAccess = arrayAccessBySwizzle[2];
+        const aAccess = arrayAccessBySwizzle[3];
+
+        if (rAccess !== -1)
+				  data[ i ] = dataEntry[rAccess];
+        if (gAccess !== -1)
+          data[ i + 1 ] = dataEntry[gAccess];
+        if (bAccess !== -1)
+          data[ i + 2 ] = dataEntry[bAccess];
+        if (aAccess !== -1)
+          data[ i + 3 ] = dataEntry[aAccess];
+			}
+
+			context.putImageData( imageData, 0, 0 );
+			return canvas;
+
+		} else if ( image.data ) {
+			const data = image.data.slice( 0 );
+
+			for ( let i = 0; i < data.length; i ++ ) {
+				if ( data instanceof Uint8Array || data instanceof Uint8ClampedArray ) {
+					data[ i ] = Math.floor( SRGBToLinear( data[ i ] / 255 ) * 255 );
+				} else {
+					// assuming float
+					data[ i ] = SRGBToLinear( data[ i ] );
+				}
+			}
+
+			return {
+				data: data,
+				width: image.width,
+				height: image.height
+			};
+		} else {
+			console.warn( 'THREE.ImageUtils.sRGBToLinear(): Unsupported image type. No color space conversion applied.' );
+			return image;
+		}
+	}
 
   assignProperty(mainMaterial, parameterName) {
     const materialParameterName = HydraMaterial.usdPreviewToMeshPhysicalMap[parameterName];
@@ -462,7 +612,7 @@ class HydraMaterial {
         if (materialParameterName === 'opacity' && mainMaterial[parameterName] < 1.0) {
           this._material.transparent = true;
         }
-        if (parameterName == 'opacityThreshold') {
+        if (parameterName == 'opacityThreshold' && mainMaterial[parameterName] > 0.0) {
           this._material.transparent = false;
           this._material.alphaClip = true;
         }
@@ -470,7 +620,7 @@ class HydraMaterial {
     }
   }
 
-  updateFinished(type, relationships) {
+  async updateFinished(type, relationships) {
     for (let relationship of relationships) {
       relationship.nodeIn = this._nodes[relationship.inputId];
       relationship.nodeOut = this._nodes[relationship.outputId];
@@ -497,12 +647,47 @@ class HydraMaterial {
     // TODO: Ideally, we don't recreate the material on every update.
     // Creating a new one requires to also update any meshes that reference it. So we're relying on the C++ side to
     // call this before also calling `setMaterial` on the affected meshes.
-    console.log('Creating Material: ' + this._id);
     this._material = new THREE.MeshPhysicalMaterial({});
+    // split _id
+    let _name = this._id;
+    let lastSlash = _name.lastIndexOf('/');
+    if (lastSlash >= 0)
+      _name = _name.substring(lastSlash + 1);
+    this._material.name = _name;
 
     // Assign textures
+    const haveRoughnessMap = !!(mainMaterialNode.roughness && mainMaterialNode.roughness.nodeIn);
+    const haveMetalnessMap = !!(mainMaterialNode.metallic && mainMaterialNode.metallic.nodeIn);
+    const haveOcclusionMap = !!(mainMaterialNode.occlusion && mainMaterialNode.occlusion.nodeIn);
+
+    console.log('Creating Material: ' + this._id, mainMaterialNode, {
+      haveRoughnessMap,
+      haveMetalnessMap,
+      haveOcclusionMap
+    });
+
+    const texturePromises = [];
     for (let key in HydraMaterial.usdPreviewToMeshPhysicalTextureMap) {
-      this.assignTexture(mainMaterialNode, key);
+      texturePromises.push(this.assignTexture(mainMaterialNode, key));
+    }
+    await Promise.all(texturePromises);
+
+    // Need to sanitize metallic/roughness/occlusion maps - if we want to export glTF they need to be identical right now
+    if (haveRoughnessMap && !haveMetalnessMap)
+    {
+      if (debugMaterials) console.log(this._material.roughnessMap, this._material);
+      this._material.metalnessMap = this._material.roughnessMap;
+      this._material.metalnessMap.needsUpdate = true;
+    }
+    else if (haveMetalnessMap && !haveRoughnessMap)
+    {
+      this._material.roughnessMap = this._material.metalnessMap;
+      this._material.roughnessMap.needsUpdate = true;
+    }
+    else if (haveMetalnessMap && haveRoughnessMap) {
+      // need to merge textures
+      // TODO
+      console.warn("TODO separate metalness and roughness textures, need to be merged");
     }
 
     // Assign material properties
@@ -514,9 +699,17 @@ class HydraMaterial {
       this._material.envMap = window.envMap;
     }
 
-    console.log("Material Node", mainMaterialNode, "Resulting Material", this._material);
+    console.log("Material Node " + this._material.name, mainMaterialNode, "Resulting Material", this._material);
   }
 }
+
+/*
+class SdfPath {
+  get name() { return this.GetName(); }
+  get absoluteRootPath() { return this.AbsoluteRootPath(); }
+  get reflexiveRelativePath() { return this.ReflexiveRelativePath(); }
+}
+*/
 
 export class RenderDelegateInterface {
 
@@ -527,8 +720,15 @@ export class RenderDelegateInterface {
     this.meshes = {};
   }
 
+  /**
+   * Render Prims. See webRenderDelegate.h and webRenderDelegate.cpp
+   * @param {string} typeId // translated from TfToken
+   * @param {string} id // SdfPath.GetAsString()
+   * @param {*} instancerId
+   * @returns 
+   */
   createRPrim(typeId, id, instancerId) {
-    if (debugPrims) console.log('Creating RPrim: ', typeId, id);
+    if (debugPrims) console.log('Creating RPrim: ', typeId, id, typeof id);
     let mesh = new HydraMesh(id, this);
     this.meshes[id] = mesh;
     return mesh;
