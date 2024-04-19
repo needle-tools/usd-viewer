3bdab212d781983637e9e7a1acb75450ce4af03d
hybridherbst <felix.herbst@gmail.com>
2024-04-19 20:41:52 +0200

    add same-site CORP header

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ fastify.register(require('@fastify/static'), {
     
     res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   },
 })
 
