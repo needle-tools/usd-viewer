4173a2f9f6548fa5c38f52b34b5b937a0bb7bba8
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-10 19:28:46 +0200

    Try load embedded wasm

--- usd-wasm/src/bindings/index.js
+++ usd-wasm/src/bindings/index.js
@@ -1,11 +1,46 @@
 import { getUsdModule as _getUsdModule } from "./emHdBindings";
 
+import base64 from "base64-js";
+
+// See https://github.com/dimforge/rapier.js/blob/master/rapier-compat/src3d/init.ts#L11
+// @ts-ignore
+// import wasmUrl from "./emHdBindings.wasm?url";
+// // @ts-ignore
+// import dataUrl from "./emHdBindings.data?url";
+
 /**
- * @param {undefined | { mainScriptUrlOrBlob: string }} opts
+ * @param {undefined | { mainScriptUrlOrBlob?: string | Blob, wasmBinary?:Buffer, locateFile?: (file:string) => string }} opts
  */
-export function getUsdModule(opts) {
+export async function getUsdModule(opts) {
+
+
+    // const baseurl = new URL(wasmUrl, import.meta.url).href;
+    // console.log(baseurl);
+
+    // const wasm = await fetch(wasmUrl).then(res => res.arrayBuffer());
+    // const data = await fetch(dataUrl).then(res => res.arrayBuffer());
+
+
+
+    // const wasmBuffer = await base64.toByteArray(wasmUrl).buffer
+
+    // const blob = new Blob([`
+    //     importScripts("./emHdBindings.js");
+    //     `,
+    // ], { type: "application/javascript" });
+    // console.log(blob);
+
     return _getUsdModule({
-        mainScriptUrlOrBlob: "/emHdBindings.js",
+        mainScriptUrlOrBlob: "./emHdBindings.js",
+        // wasmBinary: wasm,
+        // locateFile: (file) => {
+        //     console.log(file)
+        //     return "http://localhost:5174/@fs/C:/git/usd-viewer/usd-wasm/src/bindings/" + file;
+        // },
+        // getPreloadedPackage(name, size) {
+        //     console.log("PRELOAD", name, size, data)
+        //     return data;
+        // },
         ...opts,
     });
 }
