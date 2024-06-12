99fe0f2cb242362af8d0aac169e717b7d225d455
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-12 16:19:34 +0200

    remove access control methods

--- usd-wasm/src/vite/index.js
+++ usd-wasm/src/vite/index.js
@@ -4,8 +4,6 @@ const crossOriginIsolatedPlugin = {
     name: 'needle:usd-crossoriginisolated',
     configureServer: (server) => {
         server.middlewares.use((_req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Methods", "GET");
             res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
             res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
             next();
