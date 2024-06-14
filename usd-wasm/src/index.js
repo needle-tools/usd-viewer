a275e2facaba5867ad85a43990008310987a4b2f
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-15 00:25:12 +0200

    export determine file format method

--- usd-wasm/src/index.js
+++ usd-wasm/src/index.js
@@ -2,3 +2,4 @@ export * from "./bindings/index.js";
 export * from "./hydra/index.js"
 export * from "./create.three.js"
 export * from "./vite/index.js"
+export { tryDetermineFileFormat } from "./utils.js"
