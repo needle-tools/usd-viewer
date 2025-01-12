13bd33b43fab5b848bb4b89a32e9fa624f813751
Marcel Wiessler <marcel@gaisterhand.de>
2025-01-12 11:44:58 +0100

    detect USDA

--- usd-wasm/src/utils.js
+++ usd-wasm/src/utils.js
@@ -16,9 +16,9 @@ export function tryDetermineFileFormat(buffer) {
     if (bytes[0] == 80 && bytes[1] == 88 && bytes[2] == 82 && bytes[3] == 45 && bytes[4] == 85 && bytes[5] == 83 && bytes[6] == 68 && bytes[7] == 67) {
         return "usd";
     }
-    // if (bytes[0] == 117 && bytes[1] == 115 && bytes[2] == 100 && bytes[3] == 97) {
-    //     return "usda";
-    // }
+    if (bytes[0] === 35 && bytes[1] === 117 && bytes[2] === 115 && bytes[3] === 100) {
+        return "usda";
+    }
 
     return "unknown";
 }
