3271991a006e2f7390df2b0a0ce33ab547f9019e
Marcel Wiessler <marcel@gaisterhand.de>
2025-05-14 19:56:05 +0700

    Exports change to prevent multiple needle-engine imports for plugin reg(?)

--- usd-wasm/src/index.js
+++ usd-wasm/src/index.js
@@ -2,4 +2,5 @@ export * from "./bindings/index.js";
 export * from "./hydra/index.js";
 export * from "./create.three.js";
 export * from "./vite/index.js";
+export * from "./plugins/index.js";
 export { tryDetermineFileFormat } from "./utils.js"
