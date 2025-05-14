1ebb274d48537a009dd0118ef11230af15426be2
Marcel Wiessler <marcel@gaisterhand.de>
2025-05-14 18:45:43 +0700

    Dont export "plugins" from index.js - it has a custom export for clientside only

--- usd-wasm/src/index.js
+++ usd-wasm/src/index.js
@@ -2,5 +2,4 @@ export * from "./bindings/index.js";
 export * from "./hydra/index.js";
 export * from "./create.three.js";
 export * from "./vite/index.js";
-export * from "./plugins/index.js";
 export { tryDetermineFileFormat } from "./utils.js"
